a0dd1fd80962cdb6e08d44240f5f7a9cc4dcfc46
Joe Calleya <jcalleya@gmail.com>
2020-11-12 18:34:03 +0000

    reformatted so its less offensive, indented code

--- main.js
+++ main.js
@@ -6,16 +6,16 @@ const calc = () => {
     const expenses = document.querySelector('#expenses');
     const savings = document.querySelector('#savings');
     const interestRate = document.querySelector('#interestRate');
-    const interestRateDecimal = interestRate.value/ 100;
+    const interestRateDecimal = interestRate.value / 100;
     const targetSavings = 25 * expenses.value;
     const yearlySaving = salary.value - expenses.value;
 
-//calculation for time to save from the internet
-    const timeToSave = 
-    
-    Math.round([1/(Math.log(1+interestRateDecimal))]*[Math.log(yearlySaving+targetSavings*interestRateDecimal)-Math.log(yearlySaving+savings.value*interestRateDecimal)]);
+    //calculation for time to save from the internet
+    const timeToSave =
 
-//return to display
+        Math.round([1 / (Math.log(1 + interestRateDecimal))] * [Math.log(yearlySaving + targetSavings * interestRateDecimal) - Math.log(yearlySaving + savings.value * interestRateDecimal)]);
+
+    //return to display
     display.innerHTML = ``;
     display.innerHTML += `<br> 30x expenses rule target savings:<span> £${targetSavings}</span>`;
     display.innerHTML += `<br> starting with  <span> £${savings.value}</span>`;
